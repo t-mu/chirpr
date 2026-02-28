586ee425b4fa67d43c7f3a414a4f821d16a9502d
Teemu Mäntyharju <teemu.mantyharju@gmail.com>
2026-02-28 14:30:34 +0200

    chore(deploy): switch to github pages static adapter and node 24

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
+
+const base = process.env.BASE_PATH ?? '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html'
+		}),
+		paths: {
+			base
+		}
 	}
 };
 
